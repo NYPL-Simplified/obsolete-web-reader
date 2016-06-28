78666ead4e3113691695ba970464a842d51dab16
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-06-28 15:00:29 -0400

    implemented basic getPosition() and loadPosition()

--- public/js/pages.js
+++ public/js/pages.js
@@ -1,27 +1,42 @@
 var Pages = (function () {
     function Pages(_a) {
         var _b = _a === void 0 ? {} : _a, _c = _b.margin, margin = _c === void 0 ? 50 : _c, _d = _b.layoutPages, layoutPages = _d === void 0 ? 1 : _d, _e = _b.fontSize, fontSize = _e === void 0 ? 30 : _e;
+        this.margin = margin;
+        this.currentPage = 0;
+        this.currentPosition = { textNodeIndex: 0, charIndex: 0 };
         this.body = document.getElementsByTagName("body").item(0);
         this.body.style.position = "relative";
         this.body.style.overflow = "hidden";
+        this.body.style.margin = this.margin + "px";
         this.body.style.transition = "left 0.5s";
-        this.body.style.fontSize = fontSize + "px";
-        this.margin = margin;
-        this.layoutPages = layoutPages;
-        this.currentPage = 0;
-        this.setupColumns();
+        this.textNodes = this.getTextNodes(this.body);
+        this.range = document.createRange();
+        this.setFontSize(fontSize);
+        this.setLayoutPages(layoutPages);
+        this.render();
     }
-    Pages.prototype.setupColumns = function () {
-        this.pageWidth = Math.floor(window.innerWidth / this.layoutPages);
+    Pages.prototype.render = function (width, height) {
+        width = width || window.innerWidth;
+        height = height || window.innerHeight;
+        this.pageWidth = Math.floor(width / this.layoutPages);
         this.columnWidth = this.pageWidth - 2 * this.margin;
         this.columnGap = 2 * this.margin;
-        this.bodyHeight = window.innerHeight - 2 * this.margin;
-        this.body.style.margin = this.margin + "px";
+        this.bodyHeight = height - 2 * this.margin;
+        this.updateBodyStyle();
+        this.lastPage = Math.ceil(this.body.scrollWidth / this.pageWidth) - 1;
+        console.log("setup " + this.displayPage(this.lastPage) + " pages with " + this.layoutPages + " columns");
+    };
+    Pages.prototype.updateBodyStyle = function () {
         this.body.style.height = this.bodyHeight + "px";
         this.body.style.columnWidth = this.columnWidth + "px";
         this.body.style.columnGap = this.columnGap + "px";
-        this.lastPage = Math.ceil(this.body.scrollWidth / this.pageWidth) - 1;
-        console.log("setup " + this.displayPage(this.lastPage) + " pages with " + this.layoutPages + " columns");
+        this.body.style.fontSize = this.fontSize + "px";
+    };
+    Pages.prototype.setFontSize = function (size) {
+        this.fontSize = size;
+    };
+    Pages.prototype.setLayoutPages = function (num) {
+        this.layoutPages = num;
     };
     Pages.prototype.prevPage = function () {
         return Math.max(0, this.currentPage - this.layoutPages);
@@ -41,6 +56,7 @@ var Pages = (function () {
     Pages.prototype.goToPage = function (n) {
         this.body.style.left = -n * this.pageWidth + "px";
         this.currentPage = n;
+        this.saveCurrentPosition();
         console.log("moved to page " + n);
     };
     Pages.prototype.goToParagraph = function (n) {
@@ -51,28 +67,84 @@ var Pages = (function () {
         var box = para.getBoundingClientRect();
         return this.currentPage + Math.floor(box.left / (this.pageWidth * this.layoutPages));
     };
-    Pages.prototype.getParagraphFromPage = function (n) {
-        var minLeft = n * this.pageWidth;
-        var ary = Array.prototype.slice.call(document.querySelectorAll("p"));
-        return 0;
+    Pages.prototype.getTextNodes = function (element) {
+        var _this = this;
+        var childNodes = Array.prototype.slice.call(element.childNodes);
+        if (childNodes.length === 0) {
+            return element.nodeType === Node.TEXT_NODE &&
+                element.textContent.trim().length ?
+                [element] : [];
+        }
+        else {
+            return [].concat.apply([], childNodes.map(function (node) { return _this.getTextNodes(node); }));
+        }
+    };
+    Pages.prototype.getTextNodeRect = function (node) {
+        this.range.setStart(node, 0);
+        this.range.setEnd(node, node.textContent.length - 1);
+        return this.range.getBoundingClientRect();
+    };
+    Pages.prototype.getTextNodeIndex = function () {
+        var _this = this;
+        return this.textNodes.findIndex(function (textNode) {
+            return _this.getTextNodeRect(textNode).left > 0;
+        });
     };
     Pages.prototype.getPosition = function () {
-        return {
-            paragraphIndex: this.getParagraphFromPage(this.currentPage),
-            text: null,
-            textIndex: null
-        };
+        var foundIndex = this.getTextNodeIndex();
+        if (foundIndex === -1) {
+            throw ("Can't find paragraph starting on page " + this.currentPage + " or higher");
+        }
+        if (foundIndex === 0) {
+            return { textNodeIndex: 0, charIndex: 0 };
+        }
+        var textNodeIndex = -1;
+        var charIndex = -1;
+        search: for (var i = foundIndex - 1; i <= foundIndex; i++) {
+            var textNode = this.textNodes[i];
+            charIndex = 0;
+            for (var j = 0; j < textNode.textContent.length; j++) {
+                this.range.setStart(textNode, j);
+                this.range.setEnd(textNode, j + 1);
+                var left = this.range.getBoundingClientRect().left;
+                if (0 < left && left < this.pageWidth) {
+                    textNodeIndex = i;
+                    break search;
+                }
+                charIndex += 1;
+            }
+        }
+        if (textNodeIndex === -1) {
+            throw ("Can't find any text in text nodes " + (textNodeIndex - 1) + " or " + textNodeIndex + " on page " + this.currentPage);
+        }
+        return { textNodeIndex: textNodeIndex, charIndex: charIndex };
     };
-    Pages.prototype.loadPosition = function (position) {
+    Pages.prototype.saveCurrentPosition = function () {
+        this.currentPosition = this.getPosition();
+    };
+    Pages.prototype.loadPosition = function (_a) {
+        var textNodeIndex = _a.textNodeIndex, charIndex = _a.charIndex;
+        var textNode = this.textNodes[textNodeIndex];
+        this.range.setStart(textNode, charIndex);
+        this.range.setEnd(textNode, charIndex + 1);
+        var left = this.range.getBoundingClientRect().left;
+        var page = this.currentPage + Math.floor(left / this.pageWidth);
+        this.goToPage(page);
     };
     return Pages;
 }());
+var pages;
 document.addEventListener("DOMContentLoaded", function () {
-    var pages = new Pages();
+    pages = new Pages();
+    var resizeTimer;
+    var that = this;
     window.addEventListener("resize", function (e) {
-        var position = pages.getPosition();
-        pages.setupColumns();
-        pages.loadPosition(position);
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+            pages.render();
+            pages.loadPosition(pages.currentPosition);
+            pages.saveCurrentPosition();
+        }, 250);
     });
     window.addEventListener("keydown", function (e) {
         switch (e.key) {
--- src/pages.ts
+++ src/pages.ts
@@ -1,12 +1,13 @@
 interface PositionData {
-  paragraphIndex: number;
-  text: string;
-  textIndex: number;
+  textNodeIndex: number;
+  charIndex: number;
 }
 
 class Pages {
   body: HTMLElement;
+  textNodes: Element[];
   margin: number;
+  fontSize: number;
   layoutPages: number;
   pageWidth: number;
   columnWidth: number;
@@ -15,35 +16,56 @@ class Pages {
   lastPage: number;
   currentPage: number;
   currentPosition: PositionData;
+  range: Range;
 
   constructor({ margin = 50, layoutPages = 1, fontSize = 30} = {}) {
+    this.margin = margin;
+    this.currentPage = 0;
+    this.currentPosition = { textNodeIndex: 0, charIndex: 0 };
+
     this.body = document.getElementsByTagName("body").item(0);
     this.body.style.position = "relative";
     this.body.style.overflow = "hidden";
+    this.body.style.margin = this.margin + "px";
     this.body.style.transition = "left 0.5s";
-    this.body.style.fontSize = fontSize + "px";
 
-    this.margin = margin;
-    this.layoutPages = layoutPages;
-    this.currentPage = 0;
+    this.textNodes = this.getTextNodes(this.body);
+    this.range = document.createRange();
 
-    this.setupColumns();
+    this.setFontSize(fontSize);
+    this.setLayoutPages(layoutPages);
+    this.render();
   }
 
-  setupColumns(): void {
-    this.pageWidth = Math.floor(window.innerWidth / this.layoutPages);
+  render(width?: number, height?: number): void {
+    width = width || window.innerWidth;
+    height = height || window.innerHeight;
+
+    this.pageWidth = Math.floor(width / this.layoutPages);
     this.columnWidth = this.pageWidth - 2 * this.margin;
     this.columnGap = 2 * this.margin;
-    this.bodyHeight = window.innerHeight - 2 * this.margin;
+    this.bodyHeight = height - 2 * this.margin;
 
-    this.body.style.margin = this.margin + "px";
-    this.body.style.height = this.bodyHeight + "px";
-    this.body.style.columnWidth = this.columnWidth + "px";
-    this.body.style.columnGap =  this.columnGap + "px";
+    this.updateBodyStyle();
 
     this.lastPage = Math.ceil(this.body.scrollWidth / this.pageWidth) - 1;
 
     console.log(`setup ${this.displayPage(this.lastPage)} pages with ${this.layoutPages} columns`);
+  }
+
+  updateBodyStyle(): void {
+    this.body.style.height = this.bodyHeight + "px";
+    this.body.style.columnWidth = this.columnWidth + "px";
+    this.body.style.columnGap =  this.columnGap + "px";
+    this.body.style.fontSize = this.fontSize + "px";
+  }
+
+  setFontSize(size: number): void {
+    this.fontSize = size;
+  }
+
+  setLayoutPages(num: number): void {
+    this.layoutPages = num;
   }
 
   prevPage(): number {
@@ -69,6 +91,7 @@ class Pages {
   goToPage(n: number): void {
     this.body.style.left = -n * this.pageWidth + "px";
     this.currentPage = n;
+    this.saveCurrentPosition();
     console.log(`moved to page ${n}`);
   }
 
@@ -82,34 +105,99 @@ class Pages {
     return this.currentPage + Math.floor(box.left / (this.pageWidth * this.layoutPages));
   }
 
-  getParagraphFromPage(n: number): number {
-    let minLeft = n * this.pageWidth;
-    let ary = Array.prototype.slice.call(document.querySelectorAll("p"));
-    return 0;
-    // return ary.findIndex(p => {
-    //   let
-    // })
+  getTextNodes(element: Element): Element[] {
+    let childNodes = Array.prototype.slice.call(element.childNodes);
+
+    if (childNodes.length === 0) {
+      return element.nodeType === Node.TEXT_NODE &&
+             element.textContent.trim().length ?
+             [element] : [];
+    } else {
+      return [].concat.apply([], childNodes.map(node => this.getTextNodes(node)));
+    }
+  }
+
+  getTextNodeRect(node: Element): ClientRect {
+    this.range.setStart(node, 0);
+    this.range.setEnd(node, node.textContent.length - 1);
+    return this.range.getBoundingClientRect();
+  }
+
+  getTextNodeIndex(): number {
+    return (this.textNodes as any).findIndex(textNode => {
+      return this.getTextNodeRect(textNode).left > 0;
+    });
   }
 
   getPosition(): PositionData {
-    return {
-      paragraphIndex: this.getParagraphFromPage(this.currentPage),
-      text: null,
-      textIndex: null
-    };
+    // find first text node with bounding box whose left edge is on the
+    // current page or a higher page
+    let foundIndex = this.getTextNodeIndex();
+
+    if (foundIndex === -1) {
+      throw(`Can't find paragraph starting on page ${this.currentPage} or higher`);
+    }
+
+    if (foundIndex === 0) {
+      return { textNodeIndex: 0, charIndex: 0 };
+    }
+
+    // search in previous paragraph and found paragraph for text located
+    // within current page
+    let textNodeIndex = -1;
+    let charIndex = -1;
+
+    search:
+    for (let i = foundIndex - 1; i <= foundIndex; i++) {
+      let textNode = this.textNodes[i];
+      charIndex = 0;
+      for (let j = 0; j < textNode.textContent.length; j++) {
+        this.range.setStart(textNode, j);
+        this.range.setEnd(textNode, j + 1);
+        let left = this.range.getBoundingClientRect().left;
+        if (0 < left && left < this.pageWidth) {
+          textNodeIndex = i;
+          break search;
+        }
+        charIndex += 1;
+      }
+    }
+
+    if (textNodeIndex === -1) {
+      throw(`Can't find any text in text nodes ${textNodeIndex - 1} or ${textNodeIndex} on page ${this.currentPage}`);
+    }
+
+    return { textNodeIndex, charIndex };
   }
 
-  loadPosition(position: PositionData): void {
+  saveCurrentPosition() {
+    this.currentPosition = this.getPosition();
+  }
+
+  loadPosition({ textNodeIndex, charIndex }: PositionData): void {
+    let textNode = this.textNodes[textNodeIndex];
+    this.range.setStart(textNode, charIndex);
+    this.range.setEnd(textNode, charIndex + 1);
+    let left = this.range.getBoundingClientRect().left;
+    let page = this.currentPage + Math.floor(left / this.pageWidth);
+    this.goToPage(page);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  let pages = new Pages();
+let pages;
 
+document.addEventListener("DOMContentLoaded", function() {
+  pages = new Pages();
+
+  let resizeTimer;
+  let that = this;
   window.addEventListener("resize", function(e) {
-    let position = pages.getPosition();
-    pages.setupColumns();
-    pages.loadPosition(position);
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      pages.render();
+      pages.loadPosition(pages.currentPosition);
+      pages.saveCurrentPosition();
+    }, 250);
   });
 
   window.addEventListener("keydown", function(e) {
